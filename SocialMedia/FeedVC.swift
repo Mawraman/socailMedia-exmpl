57783d8cea36e69baca4e50b39e8123dc853142f
Oguzhan Akbudak <mawra@Oguzhan-MacBook-Pro.local>
2017-04-28 22:27:31 +0300

    firebase get comp

--- SocialMedia/FeedVC.swift
+++ SocialMedia/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView:UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,7 +24,16 @@ class FeedVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             print(snapshot.value!)
-            
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot]{
+                for snap in snapshots{
+                    if let postDict = snap.value as? Dictionary<String,Any>{
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()  
         })
         
     }
@@ -40,10 +51,13 @@ class FeedVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
     }
 
